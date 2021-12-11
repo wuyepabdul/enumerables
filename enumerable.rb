853f006d6d1bd6c3d7386683309a8304dd2c1940
wuyepabdul <wuyepabdul@gmail.com>
2021-12-11 20:32:39 +0100

    Fix Linters

---
+++ enumerable.rb
@@ -0,0 +1,17 @@
+module MyEnumerable
+  def all?
+    each { |element| return false unless yield element }
+    true
+  end
+
+  def any?
+    each { |element| return true if yield element }
+    false
+  end
+
+  def filter
+    results = []
+    each { |element| results.push(element) if yield element }
+    results
+  end
+end
